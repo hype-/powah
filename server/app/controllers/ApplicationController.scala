8935fee7aae32ef91e165eb77eb9618e70ef6d22
Mikko Hirvonen <mikko.petteri.hirvonen@gmail.com>
2013-07-25 20:46:57 +0300

    Remove all views from backend. Serve view from client.

--- server/app/controllers/ApplicationController.scala
+++ server/app/controllers/ApplicationController.scala
@@ -1,9 +1,14 @@
 package controllers
 
 import play.api.mvc._
+import java.io.File
 
 class ApplicationController() extends Controller {
-  def index = Action {
-    Ok(views.html.index())
+  def html(file: String) = Action {
+    val f = new File(file)
+
+    if (f.exists())
+      Ok(scala.io.Source.fromFile(f.getCanonicalPath).mkString).as("text/html")
+    else NotFound
   }
 }
